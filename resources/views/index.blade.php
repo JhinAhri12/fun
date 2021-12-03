7da2a07950eda8bfd38d0e94ea5e4a2d33dc5396
JhinAhri12 <cyprien.rezki@hotmail.fr>
2021-12-03 17:11:21 +0100

    connexion

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -82,7 +82,7 @@
             <div class="content">
                 <div class="title m-b-md">
 
-                    Juliette
+                    test
 
                 </div>
                     <p>bonjour</p>
