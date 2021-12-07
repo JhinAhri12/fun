8dac7f56a90035bbb3ef443abf1c8cb948b3b00f
Juju5974 <juliette.verschoore@hotmail.fr>
2021-12-07 11:40:58 +0100

    Accueil1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,55 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Fun & useless</title>
+        <link href="fun.css" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <div class="logo">2FAUT</div>            
+            <nav>
+                <ul>
+                    <li><a href="./home.blade.php">Accueil</a></li>
+                    <li><a href="./index.blade.php">Reccueil</a></li>
+                    <li><a href="./contact.blade.php">Contact</a></li>
+                </ul>            
+            </nav>    
+        </header>
+        <h1>FUN FACTS ABOUT (USELESS) THINGS</h1>
+        <main>
+            <article> 
+                <h2>Perles</h2>
+                <section>
+                    <h3>Number 1</h3>
+                    <p>The world looks mighty good to me, cause Tootsie Rolls are all I see. 
+                        Whatever it is I think I see, becomes a Tootsie Roll to me!</p>
+                </section>       
+                <section>
+                    <h3>Number 2</h3>
+                    <p>Tootsie Roll how I love your chocolatey chew, Tootsie Roll I think I'm in 
+                        love with you. Whatever it is I think I see, becomes a Tootsie Roll to 
+                        me!</p>
+                </section>
+                <section>
+                    <h3>Number 3</h3>
+                    <p>Baby if you've ever wondered, wondered what ever became of me. I'm living on 
+                        the air in Cincinnati, Cincinnati WKRP. Got time and tired of packing and 
+                        unpacking.</p>
+                </section>
+                <section>
+                    <h3>Number 4</h3>
+                    <p>Town to town, up and down the dial. Maybe you and me were never meant to be, 
+                        But baby think of me once in a while. I'm in WKRP in Cincinnati.</p>
+                </section>
+            </article>
+            <aside>
+                <h3>Connexion</h3>
+            </aside>
+        </main>
+        <footer>
+            Pied-de-page
+        </footer>
+    </body>
+</html>
