3cfc6ee88f899926f938c1cfcdcc760b6fddfd39
BOoMv3 <pro.css@live.fr>
2021-12-08 16:45:21 +0100

    page d'acceuil test

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,117 +1,80 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="fr">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <meta name="Description" content="Voici FunFact un projet d'entrainement">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <title>FunFact</title>
     </head>
-    <body>
-        <div class="flex-center position-ref full-height">          
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+    <body>  
+        <header>
+            <nav>
+                <div class="container">
+                    <div class="menu">
+                        <ul class="social">
+                            <li>
+                                <a href="#" class="icon">
+                                    <i class="facebook"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="icon">
+                                <i class="discord"></i>
+                                </a>
+                            </li>    
+                        </ul>
+                        <ul class="nav-links">
+                            <li>
+                                <a href="index.blade.php">Acceuil</a>
+                            </li>
+                            <li>
+                                <a href="">Contact</a>
+                            </li>
+                            <li>
+                                <a href="#">Qui somme nous ?</a>
+                            <li>
+                                <a href="login.blade.php">Connexion</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="menu2">
+                        <i class="bar"></i>
+                    </div>
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-
-                    test
-
-                </div>
-                    <p>bonjour</p>
-                    <p>bonjour</p>
-                    <p>bonjour</p>
-                    <p>bonjour</p>
-                    <p>bonjour</p>
-                    <p>bonjour</p>
-                    <p>bonjour</p><p>bonjour</p>
-                    <p>bonjour</p>
-                    <p>bonjour</p>
-                    <p>bonjour</p>
-                    <p>bonjour</p>
-                    <p>bonjour</p>
-                    <p>bonjour</p>
-                    <p>bonjour</p>
-                    <p>bonjour</p>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+            </nav>
+            <div class="glass">
+                <h1 class="one-glass">Fun</h1>
+                <div>
+                    <div>
+                        <h2>Projet</h2>
+                        <p>Un projet par nous quatres, pour vous tous !
+                        </p>
+                        <a href="#projet">
+                            suite ->
+                        </a>
+                    </div>
                 </div>
             </div>
-        </div>
+            <h1 class="no-glass">Fact</h1>
+        </header>
+
+        <section>
+            <h3 id="projet">En quoi consiste le projet ?</h3>
+            <p>
+            Non ergo erunt homines deliciis diffluentes audiendi, si quando de amicitia, quam nec usu nec ratione habent cognitam, disputabunt.
+             Nam quis est, pro deorum fidem atque hominum! qui velit, ut neque diligat quemquam nec ipse ab ullo diligatur, circumfluere omnibus copiis atque in omnium rerum abundantia 
+            </p>
+            <p>Non ergo erunt homines deliciis diffluentes audiendi, si quando de amicitia, quam nec usu nec ratione habent cognitam, disputabunt. 
+                Nam quis est, pro deorum fidem atque hominum! qui velit, ut neque diligat quemquam nec ipse ab ullo diligatur, circumfluere omnibus copiis atque in omnium rerum abundantia vivere? 
+                Haec enim est tyrannorum vita nimirum, in qua nulla fides, nulla caritas, 
+                nulla stabilis benevolentiae potest esse fiducia, omnia semper suspecta atque sollicita, nullus locus amicitiae.
+            </p>
+            <p>Non ergo erunt homines deliciis diffluentes audiendi, si quando de amicitia, quam nec usu nec ratione habent cognitam, 
+                disputabunt. Nam quis est, pro deorum fidem atque hominum! qui velit, ut neque diligat quemquam nec ipse ab ullo diligatur, 
+                circumfluere omnibus copiis atque in omnium rerum abundantia vivere? Haec enim est tyrannorum vita nimirum, in qua nulla fides, nulla caritas, nulla stabilis benevolentiae potest esse 
+                fiducia, omnia semper suspecta atque sollicita, nullus locus amicitiae.
+            </p>
+        </section>
     </body>
 </html>
